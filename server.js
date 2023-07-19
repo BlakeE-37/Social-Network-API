8a61c91767c0a31843959837ca9f05a07e2c3000
Blake H <blake.hollingsworth80@gmail.com>
2023-07-19 11:41:27 -0600

    added server.js logic

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+const db = require('./config/connection')
 
-
-
+const PORT = 3001
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(routes)
+// app.use(routes)
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`app listening on port ${PORT}`)
+    });
+});
