af081d2e54b0606659dfd46946b71e462c3fb7e1
Blake H <blake.hollingsworth80@gmail.com>
2023-07-19 12:20:00 -0600

    added user Schema and accompanying logic

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+// create the user chema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trimmed: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }]
+});
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema)
+
+module.exports = User
