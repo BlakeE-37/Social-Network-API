c091d78d764c808ce0968d4f431082b3b0388e01
Blake H <blake.hollingsworth80@gmail.com>
2023-07-20 13:24:43 -0600

    added thought schema and virtuals

---
+++ models/thought.js
@@ -0,0 +1,36 @@
+const { Schema, Model } = require('mongoose')
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: []
+
+});
+
+// getter method to format 'createdAt' date 
+thoughtSchema.virtual('formatTime').get(function () {
+    const date = new Date(this.createdAt);
+    return date.toLocaleDateString();
+})
+
+// getter method that returns the length of the reactions array
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
