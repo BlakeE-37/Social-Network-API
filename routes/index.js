20cf5b5be25fadaca76ce47b679982006315fb46
Blake H <blake.hollingsworth80@gmail.com>
2023-07-20 14:31:04 -0600

    setup routes so the root route displays "wrong route"

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
 const router = require('express').Router()
+const apiRoutes = require('./api')
+
+// router.use('/api', apiRoutes)
+
+router.use((req, res) => {
+    return res.json('Wrong route')
+});
+
+module.exports = router;
