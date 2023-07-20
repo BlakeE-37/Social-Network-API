57d1e29f1c5272af5506a05de17098a44b6e11da
Blake H <blake.hollingsworth80@gmail.com>
2023-07-20 13:39:38 -0600

    created reaction Schema

---
+++ models/reaction.js
@@ -0,0 +1,22 @@
+const { Schema, Types } = require('mongoose')
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        defualt: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
