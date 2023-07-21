3ccefa68813c9a03744fbfe9f10f697c4a483e42
Blake H <blake.hollingsworth80@gmail.com>
2023-07-21 14:49:41 -0600

    added user put and delete route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -21,6 +21,7 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// route to create a user using request body
 router.post('/', async (req, res) => {
     try {
         const data = await User.create(req.body)
@@ -28,6 +29,26 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
+// route to update a user by ID
+router.put('/:id', async (req, res) => {
+    try {
+        const data = await User.updateOne({ _id: req.params.id }, { $set: req.body })
+        res.json(data)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// route to delete user by ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const data = await User.findOneAndDelete({ _id: req.params.id })
+        res.json(data)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
