3d64acd4bad7f774373af2504e78936c3022ddc5
Blake H <blake.hollingsworth80@gmail.com>
2023-07-21 17:42:47 -0600

    added post route to create a thought and add it to a user

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Thought } = require('../../models')
+const { Thought, User } = require('../../models')
 
 // get route to retrive all thoughts
 router.get('/', async (req, res) => {
@@ -21,6 +21,20 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// post route to create a thought and add it to the users thoughts array
+router.post('/', async (req, res) => {
+    try {
+        // create the thought
+        const data = await Thought.create({ username: req.body.username, thoughtText: req.body.thoughtText });
 
+        // update the user thoughts array
+        await User.updateOne({ username: req.body.username },
+            { $push: { thoughts: data._id } })
+
+        res.json(data)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router
