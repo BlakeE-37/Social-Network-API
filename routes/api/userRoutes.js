c5f8b510bde8da762ab02b213d3bb3f847acab24
Blake H <blake.hollingsworth80@gmail.com>
2023-07-21 13:33:31 -0600

    added user route to get one user by ID

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,13 +1,24 @@
 const router = require('express').Router()
 const { User } = require('../../models')
 
+// route to get all users - no populate
 router.get('/', async (req, res) => {
     try {
-        const data = await User.find().populate('friends')
+        const data = await User.find()
         res.json(data)
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
+// route to get a User by Id - populate thoughts, and friends
+router.get('/:id', async (req, res) => {
+    try {
+        const data = await User.findById(req.params.id).populate('thoughts').populate('friends')
+        res.json(data)
+    } catch (err) {
+        res.statis(500).json(err)
+    }
+})
 
 module.exports = router;
