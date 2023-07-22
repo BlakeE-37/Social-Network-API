d3407d395282ff6dda0a0d6c702b230530d3219d
Blake H <blake.hollingsworth80@gmail.com>
2023-07-22 13:49:53 -0600

    added post and delete routes to add and remove thought reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -59,6 +59,24 @@ router.delete('/:id', async (req, res) => {
 
 // ---------- Thought Reaction Routes ----------
 
-// 
+// post route to create a reaction
+router.post('/:id/reactions', async (req, res) => {
+    try {
+        const data = await Thought.updateOne({ _id: req.params.id }, { $push: { reactions: req.body } })
+        res.json(data)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// delete route to remove a reaction from a thought
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    try {
+        const data = await Thought.updateOne({ _id: req.params.thoughtId }, { $pull: { reactions: { _id: req.params.reactionId } } })
+        res.json(data)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router
